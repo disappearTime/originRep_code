35969961b1a4692c451137433820913f365addb3
H5_zhuweiwei <zhuww@chineseall.com>
2018-03-19 23:46:16 +0800

    react环境搭建

---
+++ reactcode/reactfamily/src/components/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default class Hello extends React.Component{
+    render(){
+        return(
+            <div>hello,React</div>
+        )
+    }
+}
---
+++ reactcode/reactfamily/src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Hello from './components/Hello' // 首字母一定要大写
+ReactDom.render(<Hello />,document.getElementById('app'));
